c248dee34aeebddd4f17d9dadf691098d94700af
Rami Davidov <rami.davidov@gmail.com>
2023-07-07 20:01:18 +0300

    playing with vue3 composition api

---
+++ src/views/TestView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts" >
+
+import { ref } from 'vue'
+
+const count = ref<number>(0)
+
+const onIncrementClick = (): void => {
+    count.value++
+}
+
+const onDecrementClick = (): void => {
+    if (count.value > 0) {
+        count.value--;
+    }
+}
+
+</script>
+
+<template>
+    <div class="test">
+        <h1>COUNT {{ count }}</h1>
+        <div class="buttons">
+            <button class="increment" @click="onIncrementClick">+</button>
+            <button class="decrement" @click="onDecrementClick">_</button>
+        </div>
+    </div>
+</template>F
